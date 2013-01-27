a6f7c2295398bfda4de7cdab4f49a54af98b50ff
Julian Bauer <julianbauer@Julians-MacBook-Air.local>
2013-01-27 23:41:39 +0100

    menu_component_option_models

---
+++ server/app/database/seeds/menu_component_option_models.php
@@ -0,0 +1,101 @@
+<?php		
+
+return array (
+
+	array (
+		'id'							=> 1,
+		'menu_component_block_model_id'	=> ,
+		'category_model_id'				=> 1,
+		'title'							=> 'Sub',
+		'icon'							=> 'iconSub',
+		'smallImage'					=> 'images/static/menucomponentoptions/smallimages/sub.png',
+		'largeImage'					=> 'images/static/menucomponentoptions/largeimages/sub.png',
+		'placeholderOutline'			=> 'placeholderOutlineSub',
+		'placeholderBackground'			=> 'placeholderBackgroundSub'
+	),
+
+	array (
+		'id'							=> 2,
+		'menu_component_block_model_id'	=> ,
+		'category_model_id'				=> 2,
+		'title'							=> 'Wrap',
+		'icon'							=> 'iconWrap',
+		'smallImage'					=> 'images/static/menucomponentoptions/smallimages/wrap.png',
+		'largeImage'					=> 'images/static/menucomponentoptions/largeimages/wrap.png',
+		'placeholderOutline'			=> 'placeholderOutlineWrap',
+		'placeholderBackground'			=> 'placeholderBackgroundWrap'
+	),
+
+	array (
+		'id'							=> 3,
+		'menu_component_block_model_id'	=> ,
+		'category_model_id'				=> 3,
+		'title'							=> 'Salat',
+		'icon'							=> 'iconSalad',
+		'smallImage'					=> 'images/static/menucomponentoptions/smallimages/salat.png',
+		'largeImage'					=> 'images/static/menucomponentoptions/largeimages/salat.png',
+		'placeholderOutline'			=> 'placeholderOutlineSalad',
+		'placeholderBackground'			=> 'placeholderBackgroundSalad'
+	),
+
+	array (
+		'id'							=> 4,
+		'menu_component_block_model_id'	=> ,
+		'category_model_id'				=> 4,
+		'title'							=> 'Cookie',
+		'icon'							=> 'iconCookie',
+		'smallImage'					=> 'images/static/menucomponentoptions/smallimages/cookie.png',
+		'largeImage'					=> 'images/static/menucomponentoptions/largeimages/cookie.png',
+		'placeholderOutline'			=> 'placeholderOutlineCookie',
+		'placeholderBackground'			=> 'placeholderBackgroundCookie'
+	),
+
+	array (
+		'id'							=> 5,
+		'menu_component_block_model_id'	=> ,
+		'category_model_id'				=> 4,
+		'title'							=> 'Muffin',
+		'icon'							=> 'iconMuffin',
+		'smallImage'					=> 'images/static/menucomponentoptions/smallimages/muffin.png',
+		'largeImage'					=> 'images/static/menucomponentoptions/largeimages/muffin.png',
+		'placeholderOutline'			=> 'placeholderOutlineMuffin',
+		'placeholderBackground'			=> 'placeholderBackgroundMuffin'
+	),
+
+	array (
+		'id'							=> 6,
+		'menu_component_block_model_id'	=> ,
+		'category_model_id'				=> 4,
+		'title'							=> 'Donut',
+		'icon'							=> 'iconDonut',
+		'smallImage'					=> 'images/static/menucomponentoptions/smallimages/donut.png',
+		'largeImage'					=> 'images/static/menucomponentoptions/largeimages/donut.png',
+		'placeholderOutline'			=> 'placeholderOutlineDonut',
+		'placeholderBackground'			=> 'placeholderBackgroundDonut'
+	),
+
+	array (
+		'id'							=> 7,
+		'menu_component_block_model_id'	=> ,
+		'category_model_id'				=> 4,
+		'title'							=> 'Chips',
+		'icon'							=> 'iconChips',
+		'smallImage'					=> 'images/static/menucomponentoptions/smallimages/chips.png',
+		'largeImage'					=> 'images/static/menucomponentoptions/largeimages/chips.png',
+		'placeholderOutline'			=> 'placeholderOutlineChips',
+		'placeholderBackground'			=> 'placeholderBackgroundChips'
+	),
+
+	array (
+		'id'							=> 8,
+		'menu_component_block_model_id'	=> ,
+		'category_model_id'				=> 5,
+		'title'							=> 'Getränk',
+		'icon'							=> 'iconBeverage',
+		'smallImage'					=> 'images/static/menucomponentoptions/smallimages/getraenk.png',
+		'largeImage'					=> 'images/static/menucomponentoptions/largeimages/getraenk.png',
+		'placeholderOutline'			=> 'placeholderOutlineBeverage',
+		'placeholderBackground'			=> 'placeholderBackgroundBeverage'
+	)
+
+);
