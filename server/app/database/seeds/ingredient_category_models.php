d3ea936f12622625dc8d7e4449f9ae5cd5612764
Julian Bauer <julianbauer@Julians-MacBook-Air.local>
2013-01-27 21:58:39 +0100

    article / ingredients models

---
+++ server/app/database/seeds/ingredient_category_models.php
@@ -0,0 +1,55 @@
+<?php		
+
+return array (
+
+	array (
+		'id' 			=> 1,
+		'order' 		=> 1,
+		'title' 		=> 'Brot',
+		'smallImage'	=> 'images/static/ingredientcategories/smallimages/bread.png',
+		'icon'			=> 'iconBread',
+		'isSingle'		=> true,
+		'isMandatory'	=> true
+	),
+
+	array (
+		'id' 			=> 2,
+		'order' 		=> 2,
+		'title' 		=> 'Käse',
+		'smallImage'	=> 'images/static/ingredientcategories/smallimages/cheese.png',
+		'icon'			=> 'iconCheese',
+		'isSingle'		=> true,
+		'isMandatory'	=> true
+	),
+
+	array (
+		'id' 			=> 3,
+		'order' 		=> 3,
+		'title' 		=> 'Gemüse',
+		'smallImage'	=> 'images/static/ingredientcategories/smallimages/vegetables.png',
+		'icon'			=> 'iconVegetables',
+		'isSingle'		=> false,
+		'isMandatory'	=> false
+	),
+
+	array (
+		'id' 			=> 4,
+		'order' 		=> 4,
+		'title' 		=> 'Sauce',
+		'smallImage'	=> 'images/static/ingredientcategories/smallimages/sauce.png',
+		'icon'			=> 'iconSauce',
+		'isSingle'		=> false,
+		'isMandatory'	=> false
+	),
+
+	array (
+		'id' 			=> 5,
+		'order' 		=> 5,
+		'title' 		=> 'Extras',
+		'smallImage'	=> 'images/static/ingredientcategories/smallimages/extras.png',
+		'icon'			=> 'iconExtras'
+		'isSingle'		=> false,
+		'isMandatory'	=> false
+	),
+
+);
---
+++ server/app/database/seeds/ingredient_models.php
@@ -0,0 +1,52 @@
+<?php		
+
+return array (
+
+
+// BROTSORTEN ------------------
+// -----------------------------
+
+	array (
+		'id' 							=> 1,
+		'order' 						=> 1,
+		'ingredient_category_model_id'	=> 1,
+		'title' 						=> 'Cheese Oregano',
+		'largeImage'					=> 'images/static/ingredients/largeimages/bread/cheeseoregano.png'
+		'price'							=> 0.00
+		'isMandatory'					=> 
+	),
+
+	array (
+		'id' 							=> 2,
+		'order' 						=> 2,
+		'ingredient_category_model_id'	=> 1,
+		'title' 						=> 'Sesam',
+		'smallImage'					=> 'images/static/ingredients/largeimages/bread/sesam.png'
+	),
+
+	array (
+		'id' 							=> 3,
+		'order' 						=> 3,
+		'ingredient_category_model_id'	=> 1,
+		'title' 						=> 'Italian',
+		'smallImage'					=> 'images/static/ingredients/largeimages/bread/italian.png'
+	),
+
+	array (
+		'id' 							=> 4,
+		'order' 						=> 4,
+		'ingredient_category_model_id'	=> 1,
+		'title' 						=> 'Vollkorn',
+		'smallImage'					=> 'images/static/ingredients/largeimages/bread/vollkorn.png'
+	),
+
+	array (
+		'id' 							=> 5,
+		'order' 						=> 5,
+		'ingredient_category_model_id'	=> 1,
+		'title' 						=> 'Honey Oat',
+		'smallImage'					=> 'images/static/ingredients/largeimages/bread/honeyoat.png'
+	),
+
+
+);
