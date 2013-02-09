a744b34e489b8d7b0bcc578db0d7aa1fd3e4d3fe
Johannes Schickling <johannes@Johanness-Mac-Pro.local>
2013-02-09 13:06:47 +0100

    highlighted submit button

--- client/frontend/src/js/views/client/login/MainView.js
+++ client/frontend/src/js/views/client/login/MainView.js
@@ -12,13 +12,23 @@ define([
 
 	var MainView = PageView.extend({
 
+		$number: null,
+		$password: null,
+		$submit: null,
+
 		events: {
-			'keypress #login input': 'listenForEnter',
+			'keyup #login input': 'evalKeyboard',
 			'click #loginSubmit': 'login'
 		},
 
 		initialize: function () {
+
 			this.render();
+
+			// cache dom
+			this.$number = this.$('#loginCustomerNumber');
+			this.$password = this.$('#loginPassword');
+			this.$submit = this.$('#loginSubmit');
 		},
 
 		render: function () {
@@ -28,8 +38,8 @@ define([
 		},
 
 		login: function () {
-			var number = this.$('#loginCustomerNumber').val(),
-				password = this.$('#loginPassword').val(),
+			var number = this.$number.val(),
+				password = this.$password.val(),
 				loginSucceded;
 
 			loginSucceded = authentification.login(number, password);
@@ -45,10 +55,24 @@ define([
 
 		},
 
-		listenForEnter: function(e) {
+		evalKeyboard: function (e) {
+
+			// listen for enter
 			if (e.keyCode === 13) {
 				this.login();
 			}
+
+			// highlight submit button
+			if (this.$number.val() && this.$password.val()) {
+				this.$submit.stop().animate({
+					opacity: 1
+				});
+			} else {
+				this.$submit.stop().animate({
+					opacity: 0.5
+				});
+			}
+
 		}
 
 
