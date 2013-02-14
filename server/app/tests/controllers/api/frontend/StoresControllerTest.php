fbd822868b4dc8910ea68104e023ec582ae33cd2
Johannes Schickling <johannes@Johanness-Mac-Pro.local>
2013-02-14 21:46:09 +0100

    added store model test

---
+++ server/app/tests/controllers/api/frontend/StoresControllerTest.php
@@ -0,0 +1,59 @@
+<?php namespace App\Tests\Controllers\Api\Frontend;
+
+use App\Tests\TestCase;
+
+use Hash;
+
+use App\Models\StoreModel;
+use App\Models\ClientModel;
+
+
+class StoresControllerTest extends TestCase {
+
+	public function testIndexAvaiable()
+	{
+		$response = $this->call('GET', 'api/frontend/stores');
+		$this->assertNotNull($response);
+	}
+
+	public function testIndexContent()
+	{
+		$response = $this->call('GET', 'api/frontend/stores');
+
+
+		$this->assertNotNull($response);
+	}
+
+	protected function seedDatabase()
+	{
+		$firstAddress = array(
+			'firstName' => 'Johannes',
+			'lastName' => 'Schickling',
+			'street' => 'Kaiserallee 16',
+			'streetAdditional' => '4ter Stock',
+			'postal' => 76185,
+			'city' => 'Karlsruhe',
+			'phone' => 112,
+			'email' => 'test@my-sub.de'
+			);
+
+		$firstClientModel = new ClientModel(array(
+			'number' => 1,
+			'password' => Hash::make('haallo'),
+			'email' => 'firstClient@test.de'
+			));
+		$firstClientModel->save();
+		$firstClientModel->addressModel()->create($firstAddress);
+
+		$firstStoreModel = new StoreModel(array(
+			'title' => 'Teststore 1',
+			'number' => 1,
+			'isActive' => true,
+			'isOpen' => true,
+			'client_model_id' => $firstClientModel->id,
+			'orderEmail' => 'test@test.de'
+			));
+		$firstStoreModel->save();
+	}
+
+}
---
+++ server/app/tests/models/StoreModelTest.php
@@ -0,0 +1,65 @@
+<?php namespace App\Tests\Models;
+
+use App\Tests\TestCase;
+
+use Hash;
+
+use App\Models\StoreModel;
+use App\Models\ClientModel;
+
+
+class StoreModelTest extends TestCase {
+
+	public function testCoordsOnModelCreate()
+	{
+		$storeModel = StoreModel::where('number', 1)->first();
+
+		$expectedLatitude = 47.984883;
+		$expectedLongitude = 10.182592;
+
+		$this->assertEquals($storeModel->latitude, $expectedLatitude);
+		$this->assertEquals($storeModel->longitude, $expectedLongitude);
+	}
+
+	public function testCoordsOnAddressChange()
+	{
+		$storeModel = StoreModel::where('number', 1)->first();
+		$addressModel = $storeModel->addressModel;
+
+		$addressModel->street = 'Illerstr 11';
+
+		// reload storeModel
+		$storeModel = StoreModel::find($storeModel->id);
+
+		$expectedLatitude = 47.98923;
+		$expectedLongitude = 10.1766;
+
+		$this->assertEquals($storeModel->latitude, $expectedLatitude);
+		$this->assertEquals($storeModel->longitude, $expectedLongitude);
+	}
+
+	protected function seedDatabase()
+	{
+		$address = array(
+			'street' => 'Maximilianstr 4',
+			'postal' => 87700,
+			'city' => 'Memmingen'
+			);
+
+		$clientModel = new ClientModel(array(
+			'number' => 1,
+			'password' => Hash::make('haallo'),
+			'email' => 'firstClient@test.de'
+			));
+		$clientModel->save();
+		$clientModel->addressModel()->create($address);
+
+		$storeModel = new StoreModel(array(
+			'title' => 'Teststore',
+			'number' => 1,
+			'client_model_id' => $clientModel->id
+			));
+		$storeModel->save();
+	}
+
+}
