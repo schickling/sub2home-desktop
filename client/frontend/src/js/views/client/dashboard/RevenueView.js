00a07e1e7babc1503fbd62432e8feef3b100bc90
schickling <schickling.j@gmai.com>
2013-03-07 23:30:09 +0100

    deleted edge previews

---
+++ client/frontend/src/js/views/client/dashboard/RevenueView.js
@@ -0,0 +1,68 @@
+// Filename: src/js/views/client/dashboard/RevenuessView.js
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'lib/moment',
+    'views/client/dashboard/RevenuesView',
+    'text!templates/client/dashboard/RevenuesTemplate.html'
+    ], function ($, _, Backbone, momentLib, RevenuesView, RevenuesTemplate) {
+
+	var RevenuessView = Backbone.View.extend({
+
+		template: _.template(RevenuesTemplate),
+
+		initialize: function () {
+			this._render();
+		},
+
+		_render: function () {
+			var currentMoment = moment();
+
+			var json = {
+				totalOfCurrentYear: this._getTotalOfCurrentYear(),
+				totalOfCurrentMonth: this._getTotalOfCurrentMonth(),
+				currentMonth: currentMoment.format('MMMM'),
+				currentYear: currentMoment.format('YYYY'),
+				currentYearShort: currentMoment.format('YY')
+			};
+
+			this.$el.html(this.template(json));
+
+			this._renderRevenues();
+		},
+
+		_renderRevenues: function () {
+
+		},
+
+		_getTotalOfCurrentYear: function () {
+			var totalOfCurrentYear = 0,
+				invoicesCollection;
+
+			_.each(this.collection.models, function (storeModel) {
+				invoicesCollection = storeModel.get('invoicesCollection');
+				totalOfCurrentYear += invoicesCollection.getTotalOfCurrentYear();
+			});
+
+			return totalOfCurrentYear;
+		},
+
+		_getTotalOfCurrentMonth: function () {
+			var totalOfCurrentMonth = 0,
+				invoicesCollection;
+
+			_.each(this.collection.models, function (storeModel) {
+				invoicesCollection = storeModel.get('invoicesCollection');
+				totalOfCurrentMonth += invoicesCollection.getTotalOfCurrentMonth();
+			});
+
+			return totalOfCurrentMonth;
+		}
+
+
+	});
+
+	return RevenuessView;
+
+});
