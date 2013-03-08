9cfa06de95420e71feccb3fb97f19399906eec0c
Julian Bauer <julianbauer@Julians-MacBook-Air.local>
2013-03-08 21:50:13 +0100

    ingredientModelSeeder

--- server/app/database/seeds/IngredientModelSeeder.php
+++ server/app/database/seeds/IngredientModelSeeder.php
@@ -32,7 +32,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 1,
-				'order' 						=> 1,
+				'order' 						=> 0,
 				'ingredient_category_model_id'	=> 1,
 				'title' 						=> 'Cheese Oregano',
 				'largeImage'					=> 'img/static/ingredients/largeimages/bread/cheeseoregano.png',
@@ -41,7 +41,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 2,
-				'order' 						=> 2,
+				'order' 						=> 1,
 				'ingredient_category_model_id'	=> 1,
 				'title' 						=> 'Sesam',
 				'largeImage'					=> 'img/static/ingredients/largeimages/bread/sesam.png',
@@ -50,7 +50,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 3,
-				'order' 						=> 3,
+				'order' 						=> 2,
 				'ingredient_category_model_id'	=> 1,
 				'title' 						=> 'Italian',
 				'largeImage'					=> 'img/static/ingredients/largeimages/bread/italian.png',
@@ -59,7 +59,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 4,
-				'order' 						=> 4,
+				'order' 						=> 3,
 				'ingredient_category_model_id'	=> 1,
 				'title' 						=> 'Vollkorn',
 				'largeImage'					=> 'img/static/ingredients/largeimages/bread/vollkorn.png',
@@ -68,7 +68,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 5,
-				'order' 						=> 5,
+				'order' 						=> 4,
 				'ingredient_category_model_id'	=> 1,
 				'title' 						=> 'Honey Oat',
 				'largeImage'					=> 'img/static/ingredients/largeimages/bread/honeyoat.png',
@@ -82,7 +82,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 6,
-				'order' 						=> 6,
+				'order' 						=> 5,
 				'ingredient_category_model_id'	=> 2,
 				'title' 						=> 'Scheibenkäse',
 				'largeImage'					=> 'img/static/ingredients/largeimages/cheese/scheibenkaese.png',
@@ -91,7 +91,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 7,
-				'order' 						=> 7,
+				'order' 						=> 6,
 				'ingredient_category_model_id'	=> 2,
 				'title' 						=> 'Cheddar Cheese',
 				'largeImage'					=> 'img/static/ingredients/largeimages/cheese/cheddar.png',
@@ -100,7 +100,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 8,
-				'order' 						=> 8,
+				'order' 						=> 7,
 				'ingredient_category_model_id'	=> 2,
 				'title' 						=> 'Streichkäse',
 				'largeImage'					=> 'img/static/ingredients/largeimages/cheese/streichkaese.png',
@@ -114,7 +114,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 9,
-				'order' 						=> 9,
+				'order' 						=> 8,
 				'ingredient_category_model_id'	=> 3,
 				'title' 						=> 'Eisbergsalat',
 				'largeImage'					=> 'img/static/ingredients/largeimages/vegetables/eisbergsalat.png',
@@ -123,7 +123,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 10,
-				'order' 						=> 10,
+				'order' 						=> 9,
 				'ingredient_category_model_id'	=> 3,
 				'title' 						=> 'Tomaten',
 				'largeImage'					=> 'img/static/ingredients/largeimages/vegetables/tomate.png',
@@ -132,7 +132,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 11,
-				'order' 						=> 11,
+				'order' 						=> 10,
 				'ingredient_category_model_id'	=> 3,
 				'title' 						=> 'Salatgurke',
 				'largeImage'					=> 'img/static/ingredients/largeimages/vegetables/salatgurke.png',
@@ -141,7 +141,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 12,
-				'order' 						=> 12,
+				'order' 						=> 11,
 				'ingredient_category_model_id'	=> 3,
 				'title' 						=> 'Grüne Paprika',
 				'largeImage'					=> 'img/static/ingredients/largeimages/vegetables/paprika.png',
@@ -150,7 +150,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 13,
-				'order' 						=> 13,
+				'order' 						=> 12,
 				'ingredient_category_model_id'	=> 3,
 				'title' 						=> 'Rote Zwiebeln',
 				'largeImage'					=> 'img/static/ingredients/largeimages/vegetables/zwiebel.png',
@@ -159,7 +159,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 14,
-				'order' 						=> 14,
+				'order' 						=> 13,
 				'ingredient_category_model_id'	=> 3,
 				'title' 						=> 'schwarze Oliven',
 				'largeImage'					=> 'img/static/ingredients/largeimages/vegetables/olive.png',
@@ -168,7 +168,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 15,
-				'order' 						=> 15,
+				'order' 						=> 14,
 				'ingredient_category_model_id'	=> 3,
 				'title' 						=> 'Jalapeños (scharf)',
 				'largeImage'					=> 'img/static/ingredients/largeimages/vegetables/jalapeno.png',
@@ -177,7 +177,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 16,
-				'order' 						=> 16,
+				'order' 						=> 15,
 				'ingredient_category_model_id'	=> 3,
 				'title' 						=> 'Essiggurken',
 				'largeImage'					=> 'img/static/ingredients/largeimages/vegetables/essiggurke.png',
@@ -191,7 +191,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 17,
-				'order' 						=> 17,
+				'order' 						=> 16,
 				'ingredient_category_model_id'	=> 4,
 				'title' 						=> 'Extra Portion Fleisch',
 				'largeImage'					=> 'img/static/ingredients/largeimages/extras/extrafleisch.png',
@@ -200,7 +200,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 18,
-				'order' 						=> 18,
+				'order' 						=> 17,
 				'ingredient_category_model_id'	=> 4,
 				'title' 						=> 'Knuspriger Bacon',
 				'largeImage'					=> 'img/static/ingredients/largeimages/extras/bacon.png',
@@ -209,7 +209,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 19,
-				'order' 						=> 19,
+				'order' 						=> 18,
 				'ingredient_category_model_id'	=> 4,
 				'title' 						=> 'Doppelt Käse',
 				'largeImage'					=> 'img/static/ingredients/largeimages/extras/doppeltkaese.png',
@@ -218,7 +218,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 20,
-				'order' 						=> 20,
+				'order' 						=> 19,
 				'ingredient_category_model_id'	=> 4,
 				'title' 						=> 'Doppelt belegt',
 				'largeImage'					=> 'img/static/ingredients/largeimages/extras/doppeltbelegt.png',
@@ -227,7 +227,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 21,
-				'order' 						=> 21,
+				'order' 						=> 20,
 				'ingredient_category_model_id'	=> 4,
 				'title' 						=> 'Oregano',
 				'largeImage'					=> 'img/static/ingredients/largeimages/extras/oregano.png',
@@ -236,7 +236,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 22,
-				'order' 						=> 22,
+				'order' 						=> 21,
 				'ingredient_category_model_id'	=> 4,
 				'title' 						=> 'Salz',
 				'largeImage'					=> 'img/static/ingredients/largeimages/extras/salz.png',
@@ -245,7 +245,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 23,
-				'order' 						=> 23,
+				'order' 						=> 22,
 				'ingredient_category_model_id'	=> 4,
 				'title' 						=> 'Pfeffer',
 				'largeImage'					=> 'img/static/ingredients/largeimages/extras/pfeffer.png',
@@ -254,7 +254,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 24,
-				'order' 						=> 24,
+				'order' 						=> 23,
 				'ingredient_category_model_id'	=> 4,
 				'title' 						=> 'Parmesan',
 				'largeImage'					=> 'img/static/ingredients/largeimages/extras/parmesan.png',
@@ -268,7 +268,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 25,
-				'order' 						=> 25,
+				'order' 						=> 24,
 				'ingredient_category_model_id'	=> 5,
 				'title' 						=> 'Chipotle Southwest',
 				'largeImage'					=> 'img/static/ingredients/largeimages/sauces/chipotle.png',
@@ -277,7 +277,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 26,
-				'order' 						=> 26,
+				'order' 						=> 25,
 				'ingredient_category_model_id'	=> 5,
 				'title' 						=> 'Asiago Cesar',
 				'largeImage'					=> 'img/static/ingredients/largeimages/sauces/asiago.png',
@@ -286,7 +286,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 27,
-				'order' 						=> 27,
+				'order' 						=> 26,
 				'ingredient_category_model_id'	=> 5,
 				'title' 						=> 'BBQ-Sauce',
 				'largeImage'					=> 'img/static/ingredients/largeimages/sauces/barbecue.png',
@@ -295,7 +295,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 28,
-				'order' 						=> 28,
+				'order' 						=> 27,
 				'ingredient_category_model_id'	=> 5,
 				'title' 						=> 'Joghurt-Dressing',
 				'largeImage'					=> 'img/static/ingredients/largeimages/sauces/joghurt.png',
@@ -304,7 +304,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 29,
-				'order' 						=> 29,
+				'order' 						=> 28,
 				'ingredient_category_model_id'	=> 5,
 				'title' 						=> 'Sweet Onion',
 				'largeImage'					=> 'img/static/ingredients/largeimages/sauces/sweetonion.png',
@@ -313,7 +313,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 30,
-				'order' 						=> 30,
+				'order' 						=> 29,
 				'ingredient_category_model_id'	=> 5,
 				'title' 						=> 'Honey Mustard',
 				'largeImage'					=> 'img/static/ingredients/largeimages/sauces/honeymustard.png',
@@ -322,7 +322,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 31,
-				'order' 						=> 31,
+				'order' 						=> 30,
 				'ingredient_category_model_id'	=> 5,
 				'title' 						=> 'Lite Mayonaise',
 				'largeImage'					=> 'img/static/ingredients/largeimages/sauces/litemayonaise.png',
@@ -331,7 +331,7 @@ class IngredientModelSeeder extends Seeder {
 
 			array (
 				'id' 							=> 32,
-				'order' 						=> 32,
+				'order' 						=> 31,
 				'ingredient_category_model_id'	=> 5,
 				'title' 						=> 'Hot Sauce',
 				'largeImage'					=> 'img/static/ingredients/largeimages/sauces/hotsauce.png',
