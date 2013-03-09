64be2fd633b88c684c181d782bedd0ba4040856b
schickling <schickling.j@gmai.com>
2013-03-09 15:52:43 +0100

    prepared tooltips

---
+++ client/frontend/src/js/views/notifications/TooltipView.js
@@ -0,0 +1,16 @@
+// Filename: src/js/views/notifications/TooltipView
+define([
+	'jquery',
+	'underscore',
+	'backbone',
+	'text!templates/notifications/TooltipTemplate.html'
+	], function ($, _, Backbone, TooltipTemplate) {
+
+	var TooltipView = Backbone.View.extend({
+
+		
+
+	});
+
+	return TooltipView;
+});
