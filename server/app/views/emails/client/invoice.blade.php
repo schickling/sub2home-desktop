6cfeac48f01dcd1a5abe4f267a21125c87f1a5a5
Julian Bauer <julianbauer@Julians-MacBook-Air.local>
2013-03-25 15:47:11 +0100

    invoice mail text

--- server/app/views/emails/client/invoice.blade.php
+++ server/app/views/emails/client/invoice.blade.php
@@ -3,11 +3,17 @@
 	<head>
 		<meta charset="utf-8">
 	</head>
-	<body>
-		<h2>Rechnung fuer April</h2>
+	<body style="padding: 0; margin: 0; font-family: Helvetica, Arial, sans-serif;">
+		<h2>Rechnung für März 2013</h2>
  
-		<div>
-			
-		</div>
+		<p>
+			Sehr geehrter Herr Weigold,<br><br>
+
+			im Anhang finden Sie Ihre Rechnung für den Zeitraum vom 4.3.2013 - 3.4.2013.<br>
+			Vielen Dank für Ihr Vertrauen, und Sie wissen ja: Wenn Sie Fragen oder Probleme haben, sind wir nur einen Anruf entfernt.<br><br>
+
+			Freundliche Grüße, <br>
+			Ihr sub2home-Team
+		</p>
 	</body>
 </html>
