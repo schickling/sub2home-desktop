e740349ac0433aef8c403decd7edf7aaf3e499c7
Johannes Schickling <johannes@Johanness-Mac-Pro.local>
2013-04-06 20:05:46 +0200

    fixed phone number

--- client/frontend/src/js/views/store/dashboard/details/AddressView.js
+++ client/frontend/src/js/views/store/dashboard/details/AddressView.js
@@ -1,10 +1,10 @@
 // Filename: src/js/views/store/dashboard/details/AddressView.js
 define([
-	'jquery',
-	'underscore',
-	'backbone',
+    'jquery',
+    'underscore',
+    'backbone',
     'text!templates/store/dashboard/details/AddressTemplate.html'
-	], function ($, _, Backbone, AddressTemplate) {
+    ], function ($, _, Backbone, AddressTemplate) {
 
 	var AddressView = Backbone.View.extend({
 
@@ -15,7 +15,18 @@ define([
 		},
 
 		_render: function () {
-			this.$el.html(this.template(this.model.toJSON()));
+			var json = {
+				firstName: this.model.get('firstName'),
+				lastName: this.model.get('lastName'),
+				street: this.model.get('street'),
+				streetAdditional: this.model.get('streetAdditional'),
+				postal: this.model.get('postal'),
+				city: this.model.get('city'),
+				email: this.model.get('email'),
+				phone: this.model.get('phone')
+			};
+
+			this.$el.html(this.template(json));
 		}
 
 	});
