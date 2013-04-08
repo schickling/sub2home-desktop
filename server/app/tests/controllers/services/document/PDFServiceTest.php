b31f7dd724651ff767dc5773f43f7f22ac444175
Johannes Schickling <johannes@Johanness-Mac-Pro.local>
2013-04-08 22:37:15 +0200

    prepared document unit test

---
+++ server/app/tests/controllers/services/document/PDFServiceTest.php
@@ -0,0 +1,12 @@
+<?php namespace App\Tests\Controllers\Services\Document;
+
+use App\Tests\TestCase;
+
+class PDFServiceTest extends TestCase {
+
+	public function testShouldSucceed()
+	{
+		
+	}
+
+}
--- server/app/tests/controllers/services/image/DecodeQRCodeServiceTest.php
+++ server/app/tests/controllers/services/image/DecodeQRCodeServiceTest.php
@@ -4,16 +4,25 @@ use App\Tests\TestCase;
 
 class DecodeQRCodeServiceTest extends TestCase {
 
-	public function testDecodeShouldSucceedWithJPG()
+	public function testShouldSucceedWithDigitalImage()
 	{
-		$imagePath = __DIR__ . '/resources/success.jpg';
+		$imagePath = __DIR__ . '/resources/successDigital.jpg';
 		$resourceClass = $this->getResourceClass();
 		$info = $resourceClass::decodeImage($imagePath);
 
 		$this->assertEquals('6338450312769174', $info);
 	}
 
-	public function testDecodeShouldThrowExceptionWithWrongImage()
+	public function testShouldSucceedWithPhotoImage()
+	{
+		$imagePath = __DIR__ . '/resources/successPhoto.jpg';
+		$resourceClass = $this->getResourceClass();
+		$info = $resourceClass::decodeImage($imagePath);
+
+		$this->assertEquals('6338450312769174', $info);
+	}
+
+	public function testShouldThrowExceptionWithWrongImage()
 	{
 		$this->setExpectedException('App\\Exceptions\\ServiceException');
 
