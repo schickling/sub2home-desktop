621c53346720b8ca187f28dcdcdc120701a46b8d
Johannes Schickling <johannes@Johanness-Mac-Pro.local>
2013-04-25 22:39:43 +0200

    assert right order after refreshing orders

--- src/js/collections/OrdersCollection.js
+++ src/js/collections/OrdersCollection.js
@@ -1,13 +1,17 @@
 // Filename: src/js/collections/OrderCollection.js
 define([
-	'underscore',
-	'backbone',
-	'models/OrderModel'
-	], function (_, Backbone, OrderModel) {
+    'underscore',
+    'backbone',
+    'models/OrderModel'
+    ], function (_, Backbone, OrderModel) {
 
 	var OrderCollection = Backbone.Collection.extend({
 
 		model: OrderModel,
+
+		comparator: function (orderModel) {
+			return -(orderModel.id);
+		},
 
 		url: function () {
 			return 'stores/storeAlias/orders';
