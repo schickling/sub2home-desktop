78cc6de9bd3673d4e495c4d5aa7929857cf1d3c8
Johannes Schickling <schickling.j@gmail.com>
2013-09-28 11:10:41 +0200

    prepared Facebook url

---
+++ src/coffee/views/store/config/StoreInfoView.coffee
@@ -0,0 +1,106 @@
+define ["jquery", "underscore", "backbone", "services/notificationcenter", "views/store/config/AddressView", "text!templates/store/config/StoreInfoTemplate.html"], ($, _, Backbone, notificationcenter, AddressView, StoreInfoTemplate) ->
+
+  StoreInfoView = Backbone.View.extend
+
+    events:
+      "focusout #storeDescriptionInput": "_updateDescription"
+      "focusout #storeOrderingContactInput": "_updateOrderEmail"
+      "focusout #storeFacebookInput": "_updateFacebookUrl"
+      "mouseenter #bMail": "_tooltipForTestOrder"
+      "mouseleave .iBtn": "_dismissTooltip"
+      "click #bMail": "_sendTestOrder"
+      "click #storeOpen": "_toggleOpen"
+      "click #payment button.toggle": "_togglePaymentMethod" # payment methods
+
+    initialize: ->
+      @_render()
+
+    _render: ->
+      json =
+        number: @model.get("number")
+        title: @model.get("title")
+        orderEmail: @model.get("orderEmail")
+        isOpen: @model.get("isOpen")
+        facebookUrl: @model.get("facebookUrl")
+        allowsPaymentEc: @model.get("allowsPaymentEc")
+        allowsPaymentCash: @model.get("allowsPaymentCash")
+
+      @$el.html _.template(StoreInfoTemplate, json)
+      new AddressView(
+        el: @$("#storeAddress")
+        model: @model
+      )
+
+    _updateDescription: (e) ->
+      $textarea = $(e.target)
+      description = $textarea.val()
+      @model.set "description", description
+      @_saveModel()
+
+    _updateOrderEmail: (e) ->
+      $input = $(e.target)
+      val = $input.val()
+      @model.set "orderEmail", val
+      @_saveModel()
+
+    _updateFacebookUrl: (e) ->
+      $input = $(e.target)
+      val = $input.val()
+      @model.set "facebookUrl", val
+      @_saveModel()
+
+    _sendTestOrder: ->
+      $.ajax
+        url: "stores/storeAlias/testorder"
+        type: "post"
+        success: ->
+          notificationcenter.notify "views.store.config.testOrder.success"
+        error: ->
+          notificationcenter.notify "views.store.config.testOrder.error"
+
+    _toggleOpen: ->
+      $button = @$("#storeOpen")
+      isOpen = not @model.get("isOpen")
+      @model.set "isOpen", isOpen
+      @model.save {},
+        success: ->
+          if isOpen
+            notificationcenter.notify "views.store.config.isOpen"
+          else
+            notificationcenter.notify "views.store.config.isClosed"
+          $button.toggleClass "isOpen"
+        error: ->
+          notificationcenter.notify "views.store.config.isOpenError"
+
+    _togglePaymentMethod: (e) ->
+      self = this
+      storeModel = @model
+      $button = $(e.target)
+      $wrapper = $button.parent()
+      method = $button.attr("data-method")
+      attribute = "allowsPayment" + method
+      value = not @model.get(attribute)
+      changedAttributes = {}
+      changedAttributes[attribute] = value
+      @model.save changedAttributes,
+        validate: true
+        success: ->
+          notificationcenter.notify "views.store.config.paymentMethods.success"
+          $wrapper.toggleClass "disabled"
+        error: ->
+          notificationcenter.notify "views.store.config.paymentMethods.error"
+
+    _saveModel: ->
+      self = this
+      @model.save {},
+        success: ->
+          notificationcenter.notify "views.store.config.info.success"
+        error: ->
+          notificationcenter.notify "views.store.config.info.error"
+
+    _tooltipForTestOrder: ->
+      offset = @$("#bMail").offset()
+      notificationcenter.tooltip "views.store.config.testOrder", offset.top + 24, offset.left + 15
+
+    _dismissTooltip: ->
+      notificationcenter.hideTooltip()
--- src/js/views/store/config/StoreInfoView.js
+++ src/js/views/store/config/StoreInfoView.js
@@ -1,173 +1,129 @@
-// Filename: src/js/views/store/config/StoreInfoView.js
-define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'services/notificationcenter',
-    'views/store/config/AddressView',
-    'text!templates/store/config/StoreInfoTemplate.html'
-    ], function ($, _, Backbone, notificationcenter, AddressView, StoreInfoTemplate) {
-
-	"use strict";
-
-	var StoreInfoView = Backbone.View.extend({
-
-		events: {
-			'focusout #storeDescriptionInput': '_updateDescription',
-			'focusout #storeOrderingContactInput': '_updateOrderEmail',
-			'mouseenter #bMail': '_tooltipForTestOrder',
-			'mouseleave .iBtn': '_dismissTooltip',
-			'click #bMail': '_sendTestOrder',
-			'click #storeOpen': '_toggleOpen',
-			// payment methods
-			'click #payment button.toggle': '_togglePaymentMethod',
-			'click #paypalSettings': '_updatePaypal'
-		},
-
-		initialize: function () {
-			this._render();
-		},
-
-		_render: function () {
-			var json = {
-				'number': this.model.get('number'),
-				'title': this.model.get('title'),
-				'orderEmail': this.model.get('orderEmail'),
-				'isOpen': this.model.get('isOpen'),
-				'allowsPaymentPaypal': this.model.get('allowsPaymentPaypal'),
-				'allowsPaymentEc': this.model.get('allowsPaymentEc'),
-				'allowsPaymentCash': this.model.get('allowsPaymentCash')
-			};
-
-			this.$el.html(_.template(StoreInfoTemplate, json));
-
-			new AddressView({
-				el: this.$('#storeAddress'),
-				model: this.model
-			});
-		},
-
-		_updateDescription: function (e) {
-			var $textarea = $(e.target),
-				description = $textarea.val();
-
-			this.model.set('description', description);
-			this._saveModel();
-		},
-
-		_updateOrderEmail: function (e) {
-			var $input = $(e.target),
-				val = $input.val();
-
-			this.model.set('orderEmail', val);
-			this._saveModel();
-		},
-
-		_sendTestOrder: function () {
-			$.ajax({
-				url: 'stores/storeAlias/testorder',
-				type: 'post',
-				success: function () {
-					notificationcenter.notify('views.store.config.testOrder.success');
-				},
-				error: function () {
-					notificationcenter.notify('views.store.config.testOrder.error');
-				}
-			});
-		},
-
-		_toggleOpen: function () {
-			var $button = this.$('#storeOpen'),
-				isOpen = !this.model.get('isOpen');
-
-			this.model.set('isOpen', isOpen);
-			this.model.save({}, {
-				success: function () {
-					if (isOpen) {
-						notificationcenter.notify('views.store.config.isOpen');
-					} else {
-						notificationcenter.notify('views.store.config.isClosed');
-					}
-					$button.toggleClass('isOpen');
-				},
-				error: function () {
-					notificationcenter.notify('views.store.config.isOpenError');
-				}
-			});
-		},
-
-		_togglePaymentMethod: function (e) {
-			var self = this,
-				storeModel = this.model,
-				$button = $(e.target),
-				$wrapper = $button.parent(),
-				method = $button.attr('data-method'),
-				attribute = 'allowsPayment' + method,
-				value = !this.model.get(attribute),
-				changedAttributes = {};
-
-			changedAttributes[attribute] = value;
-
-			this.model.save(changedAttributes, {
-				validate: true,
-				success: function () {
-					notificationcenter.notify('views.store.config.paymentMethods.success');
-					$wrapper.toggleClass('disabled');
-				},
-				error: function () {
-					if (attribute === 'allowsPaymentPaypal') {
-						notificationcenter.notify('views.store.config.paymentMethods.error.paypal');
-						self.updatePaypal();
-					} else {
-						notificationcenter.notify('views.store.config.paymentMethods.error');
-					}
-				}
-			});
-		},
-
-		_updatePaypal: function () {
-			var url = this.model.url() + '/updatepaypal',
-				$button = this.$('#paypalSettings');
-
-			$button.prepend('Seite wird geladen...');
-
-			notificationcenter.notify('views.store.config.paymentMethods.loadPaypal');
-
-			$.ajax({
-				url: url,
-				type: 'get',
-				dataType: 'json',
-				success: function (response) {
-					var paypalUrl = response.url;
-					window.location = paypalUrl;
-				}
-			});
-
-		},
-
-		_saveModel: function () {
-			var self = this;
-			this.model.save({}, {
-				success: function () {
-					notificationcenter.notify('views.store.config.info.success');
-				},
-				error: function () {
-					notificationcenter.notify('views.store.config.info.error');
-				}
-			});
-		},
-
-		_tooltipForTestOrder: function () {
-			var offset = this.$('#bMail').offset();
-			notificationcenter.tooltip('views.store.config.testOrder', offset.top + 24, offset.left + 15);
-		},
-
-		_dismissTooltip: function () {
-			notificationcenter.hideTooltip();
-		}
-
-	});
-
-	return StoreInfoView;
-
+define(["jquery", "underscore", "backbone", "services/notificationcenter", "views/store/config/AddressView", "text!templates/store/config/StoreInfoTemplate.html"], function($, _, Backbone, notificationcenter, AddressView, StoreInfoTemplate) {
+  var StoreInfoView;
+  return StoreInfoView = Backbone.View.extend({
+    events: {
+      "focusout #storeDescriptionInput": "_updateDescription",
+      "focusout #storeOrderingContactInput": "_updateOrderEmail",
+      "focusout #storeFacebookInput": "_updateFacebookUrl",
+      "mouseenter #bMail": "_tooltipForTestOrder",
+      "mouseleave .iBtn": "_dismissTooltip",
+      "click #bMail": "_sendTestOrder",
+      "click #storeOpen": "_toggleOpen",
+      "click #payment button.toggle": "_togglePaymentMethod"
+    },
+    initialize: function() {
+      return this._render();
+    },
+    _render: function() {
+      var json;
+      json = {
+        number: this.model.get("number"),
+        title: this.model.get("title"),
+        orderEmail: this.model.get("orderEmail"),
+        isOpen: this.model.get("isOpen"),
+        facebookUrl: this.model.get("facebookUrl"),
+        allowsPaymentEc: this.model.get("allowsPaymentEc"),
+        allowsPaymentCash: this.model.get("allowsPaymentCash")
+      };
+      this.$el.html(_.template(StoreInfoTemplate, json));
+      return new AddressView({
+        el: this.$("#storeAddress"),
+        model: this.model
+      });
+    },
+    _updateDescription: function(e) {
+      var $textarea, description;
+      $textarea = $(e.target);
+      description = $textarea.val();
+      this.model.set("description", description);
+      return this._saveModel();
+    },
+    _updateOrderEmail: function(e) {
+      var $input, val;
+      $input = $(e.target);
+      val = $input.val();
+      this.model.set("orderEmail", val);
+      return this._saveModel();
+    },
+    _updateFacebookUrl: function(e) {
+      var $input, val;
+      $input = $(e.target);
+      val = $input.val();
+      this.model.set("facebookUrl", val);
+      return this._saveModel();
+    },
+    _sendTestOrder: function() {
+      return $.ajax({
+        url: "stores/storeAlias/testorder",
+        type: "post",
+        success: function() {
+          return notificationcenter.notify("views.store.config.testOrder.success");
+        },
+        error: function() {
+          return notificationcenter.notify("views.store.config.testOrder.error");
+        }
+      });
+    },
+    _toggleOpen: function() {
+      var $button, isOpen;
+      $button = this.$("#storeOpen");
+      isOpen = !this.model.get("isOpen");
+      this.model.set("isOpen", isOpen);
+      return this.model.save({}, {
+        success: function() {
+          if (isOpen) {
+            notificationcenter.notify("views.store.config.isOpen");
+          } else {
+            notificationcenter.notify("views.store.config.isClosed");
+          }
+          return $button.toggleClass("isOpen");
+        },
+        error: function() {
+          return notificationcenter.notify("views.store.config.isOpenError");
+        }
+      });
+    },
+    _togglePaymentMethod: function(e) {
+      var $button, $wrapper, attribute, changedAttributes, method, self, storeModel, value;
+      self = this;
+      storeModel = this.model;
+      $button = $(e.target);
+      $wrapper = $button.parent();
+      method = $button.attr("data-method");
+      attribute = "allowsPayment" + method;
+      value = !this.model.get(attribute);
+      changedAttributes = {};
+      changedAttributes[attribute] = value;
+      return this.model.save(changedAttributes, {
+        validate: true,
+        success: function() {
+          notificationcenter.notify("views.store.config.paymentMethods.success");
+          return $wrapper.toggleClass("disabled");
+        },
+        error: function() {
+          return notificationcenter.notify("views.store.config.paymentMethods.error");
+        }
+      });
+    },
+    _saveModel: function() {
+      var self;
+      self = this;
+      return this.model.save({}, {
+        success: function() {
+          return notificationcenter.notify("views.store.config.info.success");
+        },
+        error: function() {
+          return notificationcenter.notify("views.store.config.info.error");
+        }
+      });
+    },
+    _tooltipForTestOrder: function() {
+      var offset;
+      offset = this.$("#bMail").offset();
+      return notificationcenter.tooltip("views.store.config.testOrder", offset.top + 24, offset.left + 15);
+    },
+    _dismissTooltip: function() {
+      return notificationcenter.hideTooltip();
+    }
+  });
 });
