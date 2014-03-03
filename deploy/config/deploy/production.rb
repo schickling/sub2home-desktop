f86e45b68c94a82cba59542ced2289872332a4fe
Johannes Schickling <schickling.j@gmail.com>
2014-03-03 09:02:18 +0100

    key path

--- deploy/config/deploy/production.rb
+++ deploy/config/deploy/production.rb
@@ -3,6 +3,6 @@ set :stage, :production
 role :app, %w{www-data@54.229.26.88}
 
 set :ssh_options, {
-  keys: %w(cert/schickling.pem),
+  keys: %w(../../schickling.pem),
   auth_methods: %w(publickey)
 }
